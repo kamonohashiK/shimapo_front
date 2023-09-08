bac7e01bbe28fb03141a490b3b3705460735fc66
kamonohashiK <kamonohashiy@gmail.com>
2023-09-08 19:56:45 +0900

    #3 マーカーを複数個表示する場合のサンプル

--- app/components/islands_map.tsx
+++ app/components/islands_map.tsx
@@ -11,10 +11,13 @@ const position = {
   lng: 132.619553,
 };
 
-const testIslandPosition = {
-  lat: 33.975178,
-  lng: 132.619553,
-};
+const testIslandPositions = [
+  { lat: 33.975178, lng: 132.619553 },
+  { lat: 33.980306, lng: 132.548509 },
+  { lat: 33.969383, lng: 132.689278 },
+  { lat: 33.883688, lng: 132.673384 },
+  { lat: 33.964277, lng: 132.661154 },
+];
 
 export default function IslandsMap() {
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY;
@@ -28,7 +31,11 @@ export default function IslandsMap() {
       <div className="wrap">
         <LoadScript googleMapsApiKey={apiKey}>
           <GoogleMap mapContainerStyle={container} center={position} zoom={11}>
-            <Marker position={testIslandPosition} onClick={testFunc}/>
+            {testIslandPositions.map((position, index) => {
+              return (
+                <Marker key={index} position={position} onClick={testFunc} />
+              );
+            })}
           </GoogleMap>
         </LoadScript>
       </div>
