64d4f77cef3aa4a84a51a3c56e1527c35a2e17a0
kamonohashiK <kamonohashiy@gmail.com>
2023-09-11 20:26:01 +0900

    #8 ログインページのざっくりレイアウト + 全体の修正

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,29 @@
 import * as React from "react";
+import { Button, Typography, Stack, TextField, Container } from "@mui/material";
 
 export default function LoginPage() {
   return (
-    <div>
-      <h1>ログインページ</h1>
-    </div>
+    <Container className="content" fixed>
+      <Typography variant="h4" color="secondary">
+        Login
+      </Typography>
+      <Stack spacing={2} margin={3} marginLeft={10} className="button-group">
+        <Stack spacing={2} margin={3}>
+          <TextField label="Email" />
+          <TextField label="Password" />
+          <Button variant="outlined">ログイン</Button>
+        </Stack>
+
+        <Typography variant="h4" color="secondary">
+          Sign Up
+        </Typography>
+        <Stack spacing={2} margin={3} className="button-group">
+          <Button variant="outlined">メール</Button>
+          <Button variant="outlined">Google</Button>
+          <Button variant="outlined">Twitter</Button>
+          <Button variant="outlined">Facebook</Button>
+        </Stack>
+      </Stack>
+    </Container>
   );
 }
