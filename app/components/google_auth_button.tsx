b853cad37de1b70cb39e2c8fc602120d89ac621a
kamonohashiK <kamonohashiy@gmail.com>
2023-09-15 17:57:38 +0900

    #17 認証トークンをCookieに保存する処理を追加

--- app/components/google_auth_button.tsx
+++ app/components/google_auth_button.tsx
@@ -1,29 +1,34 @@
-//components/google_auth_button.tsx
 "use client";
 import firebase_app from "../../firebase/config";
 import { getAuth, signInWithPopup, GoogleAuthProvider, UserCredential } from "@firebase/auth";
 import { Button } from "@mui/material";
 import { useAppDispatch } from "../store/hooks";
 import { setLoginInfo } from "../store/userSlice";
+import { setAccessToken } from "../util/auth";
 
 export default function GoogleAuthButton() {
   // Google認証
   function authentication() {
     const auth = getAuth(firebase_app);
     signInWithPopup(auth, new GoogleAuthProvider())
-      .then((result) => {
+      .then(async (result) => {
         console.log("Google認証");
         const credential = GoogleAuthProvider.credentialFromResult(result);
 
-        // TODO: アクセストークンをどこかに保存(場所は要検討)
-        const token = credential.accessToken;
-        //console.log(token);
+        //アクセストークンをクッキーに保存
+        const idToken = credential?.idToken;
+        const accessToken = credential?.accessToken;
+        if (idToken && accessToken) {
+          await setAccessToken(idToken, accessToken);
 
-        // 取得したユーザーの情報(uid,displayName,photoUrl)をstoreに保存
-        const user = result.user;
-        const displayName = user.displayName ? user.displayName : "";
-        const photoUrl = user.photoURL ? user.photoURL : "";
-        setUserState(user.uid, displayName, photoUrl);
+          // 取得したユーザーの情報(uid,displayName,photoUrl)をstoreに保存
+          const user = result.user;
+          const displayName = user.displayName ? user.displayName : "";
+          const photoUrl = user.photoURL ? user.photoURL : "";
+          setUserState(user.uid, displayName, photoUrl);
+        } else {
+          throw new Error("アクセストークンが取得できませんでした。");
+        }
       })
       .catch((error) => {
         console.log("Google認証エラー");
