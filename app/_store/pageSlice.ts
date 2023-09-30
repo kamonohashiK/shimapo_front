15d8fc0912e058db58e62e35a4f091f42b66dd0a
kamonohashiK <kamonohashiy@gmail.com>
2023-09-30 14:11:42 +0900

    #22 選択された島のデータを格納する記述を追加

--- app/_store/pageSlice.ts
+++ app/_store/pageSlice.ts
@@ -2,10 +2,22 @@ import { createSlice } from "@reduxjs/toolkit";
 
 interface PageState {
     isIslandInfo: boolean;
+    uid: string;
+    name: string;
+    prefecture: string;
+    city: string;
+    kana: string;
+    enName: string;
 }
 
 const initialState: PageState = {
     isIslandInfo: false,
+    uid: "",
+    name: "島の名前",
+    prefecture: "都道府県名",
+    city: "市区町村名",
+    kana: "かな",
+    enName: "English Name",
 }
 
 export const pageSlice = createSlice({
