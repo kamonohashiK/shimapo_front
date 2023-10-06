05d9e06387940a5e11153a5feba5d44220c1cc5f
kamonohashiK <kamonohashiy@gmail.com>
2023-10-07 00:28:56 +0900

    #49 APIからのリクエストを受け取った際の処理の叩き台を作成

--- app/(routes)/login/page.tsx
+++ app/(routes)/login/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Typography, Container, Grid } from "@mui/material";
 import Sidebar from "../../_components/sidebar/sidebar";
 import EmailSignupForm from "@/app/_components/form/email_signup";
+import HeaderAlert from "@/app/_components/util/header_alert";
 
 export default function LoginPage() {
   const sidebarTitle = "ログイン";
@@ -10,11 +11,12 @@ export default function LoginPage() {
 
   return (
     <Grid container direction="row" spacing={2}>
-      <Grid item xs={3}>
+      <Grid item xs={4}>
         <Sidebar title={sidebarTitle} content={sidebarText} />
       </Grid>
-      <Grid item xs={9} id="content">
+      <Grid item xs={8} id="content">
         <Container className="content" fixed>
+          <HeaderAlert />
           <Typography variant="h4" color="secondary">
             Sign Up
           </Typography>
--- app/_components/form/email_signup.tsx
+++ app/_components/form/email_signup.tsx
@@ -1,12 +1,15 @@
 import { Stack, TextField, Button } from "@mui/material";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux/es/hooks/useDispatch";
+import { hideAlert, setAlert } from "@/app/_store/alertSlice";
+import axios from "axios";
 
 // 入力フォームの型定義
 type SignupForm = {
-    email: string;
-}
+  email: string;
+};
 
 // バリデーションルールの定義
 const schema = yup.object({
@@ -17,33 +20,74 @@ const schema = yup.object({
 });
 
 export default function EmailSignupForm() {
-    const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm<SignupForm>({
-      resolver: yupResolver(schema),
-    });
+  const dispatch = useDispatch();
 
-    const onSubmit: SubmitHandler<SignupForm> = (data) => {
-      console.log(data);
-    };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignupForm>({
+    resolver: yupResolver(schema),
+  });
 
-    return (
-      <Stack spacing={2} margin={3} marginLeft={10}>
-        <Stack spacing={2} margin={3}>
-          <TextField
-            id="email"
-            required
-            error={!!errors.email}
-            label="メールアドレス"
-            {...register("email")}
-            helperText={errors.email?.message}
-          />
-          <Button variant="outlined" onClick={handleSubmit(onSubmit)}>
-            メールアドレスを送信
-          </Button>
-        </Stack>
+  const onSubmit = (data: SignupForm) => {
+    // TODO: 環境変数からURLを取得するようにする
+    axios
+      .post("http://localhost:18000/api/v1/auth/signup", data)
+      .then((response) => {
+        if (response.status === 200) {
+          // 成功時
+          handleAlert(
+            true,
+            "送信されたメールアドレスに案内メールを送信しました。"
+          );
+        } else {
+          // 失敗時
+          handleAlert(
+            false,
+            "送信時にエラーが発生しました。時間を置いて再度お試しください。"
+          );
+        }
+      })
+      .catch(() => {
+        handleAlert(
+          false,
+          "送信時にエラーが発生しました。時間を置いて再度お試しください。"
+        );
+      });
+  };
+
+  function handleAlert(succeed: boolean, message: string) {
+    if (succeed) {
+      dispatch(
+        setAlert({ severity: "success", message: message, isShown: true })
+      );
+    } else {
+      dispatch(
+        setAlert({ severity: "error", message: message, isShown: true })
+      );
+    }
+
+    setTimeout(() => {
+      dispatch(hideAlert());
+    }, 5000);
+  }
+
+  return (
+    <Stack spacing={2} margin={3}>
+      <Stack spacing={2} margin={3}>
+        <TextField
+          id="email"
+          required
+          error={!!errors.email}
+          label="メールアドレス"
+          {...register("email")}
+          helperText={errors.email?.message}
+        />
+        <Button variant="outlined" onClick={handleSubmit(onSubmit)}>
+          送信
+        </Button>
       </Stack>
-    );
+    </Stack>
+  );
 }
