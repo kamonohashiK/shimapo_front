94db86db756cb3e57f3b6daf6c8ebc9d3ae3dfbe
kamonohashiK <kamonohashiy@gmail.com>
2023-10-08 20:47:15 +0900

    #50 Googleでの認証ボタンを作成

--- app/_components/auth/google_auth_button.tsx
+++ app/_components/auth/google_auth_button.tsx
@@ -1,11 +1,34 @@
 "use client";
 
+import firebase_app from "@/firebase/config";
 import { Button } from "@mui/material";
+import firebase, { GoogleAuthProvider, getAuth, signInWithPopup } from "@firebase/auth";
 
 export default function GoogleAuthButton() {
 
+  function SignInWithGoogle() {
+    const auth = getAuth(firebase_app);
+    signInWithPopup(auth, new GoogleAuthProvider())
+      .then((result) => {
+        //TODO: ログイン後の処理
+        console.log("Google認証");
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        //const token = credential.accessToken ?? "";
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log("Google認証エラー");
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
+  }
+
   return (
-    <Button variant="outlined" >
+    <Button variant="outlined" onClick={SignInWithGoogle}>
       Google
     </Button>
   );
