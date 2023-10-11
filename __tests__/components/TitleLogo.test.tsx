5fa3e1d840d1197da2a781551360eabcd436930d
kamonohashiK <kamonohashiy@gmail.com>
2023-10-11 12:43:44 +0900

    #66 TitleLogoのテスト項目を申し訳程度に追加

--- __tests__/components/TitleLogo.test.tsx
+++ __tests__/components/TitleLogo.test.tsx
@@ -3,9 +3,20 @@ import "@testing-library/jest-dom";
 import TitleLogo from "@/app/_components/header/title_logo";
 
 describe("TitleLogoのテスト", () => {
+  let component: ReturnType<typeof render>;
+
+  // 毎回最初にTitleLogoをレンダリングする
+  beforeEach(() => {
+    component = render(<TitleLogo />);
+  });
+
   it("しまぽという文字列が表示されている", () => {
-    render(<TitleLogo />);
     const text = screen.getByText("しまぽ");
     expect(text).toBeInTheDocument();
+  });
+
+  it("ルートへのリンクがある", () => {
+    const link = screen.getByRole("link");
+    expect(link).toHaveAttribute("href", "/");
   });
 });
