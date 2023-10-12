6f90c9639047bec027c5503efff063738a327254
kamonohashiK <kamonohashiy@gmail.com>
2023-10-13 06:59:03 +0900

    #71 高評価・低評価ボタンを押下した際のUIを作成

--- app/_components/sidebar/answers/dislike_button.tsx
+++ app/_components/sidebar/answers/dislike_button.tsx
@@ -1,16 +1,28 @@
 import { Tooltip, IconButton, Typography } from "@mui/material";
 import ThumbDownOffAltIcon from "@mui/icons-material/ThumbDownOffAlt";
 import ThumbDownAltIcon from "@mui/icons-material/ThumbDownAlt";
+import { useState } from "react";
 
 interface DislikeButtonProps {
   disliked_by: string[];
 }
 
 export default function DislikeButton(props: DislikeButtonProps) {
+  const [disliked, setDisliked] = useState(false);
+
+  // 低評価ボタンの状態を管理する(UI確認用に一時的に追加)
+  const toggleDisliked = () => {
+    setDisliked(!disliked);
+  };
+
   return (
     <Tooltip title="低評価する" placement="top">
-      <IconButton>
-        <ThumbDownOffAltIcon />
+      <IconButton onClick={toggleDisliked}>
+        {disliked ? (
+          <ThumbDownAltIcon color="error" />
+        ) : (
+          <ThumbDownOffAltIcon />
+        )}
       </IconButton>
     </Tooltip>
   );
--- app/_components/sidebar/answers/like_button.tsx
+++ app/_components/sidebar/answers/like_button.tsx
@@ -1,6 +1,7 @@
 import { Tooltip, IconButton, Typography } from "@mui/material";
 import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import { useState } from "react";
 
 interface LikeButtonProps {
   liked_by: string[];
@@ -12,11 +13,18 @@ const countLiked = (array: any[]) => {
 };
 
 export default function LikeButton(props: LikeButtonProps) {
+  const [liked, setLiked] = useState(false);
+
+  // 高評価ボタンの状態を管理する(UI確認用に一時的に追加)
+  const toggleLiked = () => {
+    setLiked(!liked);
+  };
+
   return (
     <>
       <Tooltip title="高評価する" placement="top">
-        <IconButton>
-          <ThumbUpOffAltIcon />
+        <IconButton onClick={toggleLiked}>
+          {liked ? <ThumbUpIcon color="success" /> : <ThumbUpOffAltIcon />}
         </IconButton>
       </Tooltip>
       <Typography>{countLiked(props.liked_by)}</Typography>
