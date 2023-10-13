ef8ce2a5e8b5e077ea731441edd713e2fb5b6d2a
kamonohashiK <kamonohashiy@gmail.com>
2023-10-13 15:54:40 +0900

    #75 変数名を修正

--- app/_api/island.ts
+++ app/_api/island.ts
@@ -7,24 +7,24 @@ import { IslandImageCollection } from "./collections/island_image";
 import { IslandQuestionCollection } from "./collections/question";
 
 // 島の情報を取得
-export async function getIslandInfo(uid: string) {
+export async function getIslandInfo(islandId: string) {
   try {
     // 島の情報をDBから取得
-    const islandCollection = new IslandCollection(uid);
-    const docSnap = await islandCollection.getSnapshot();
+    const islandCollection = new IslandCollection(islandId);
+    const islandData = await islandCollection.getData();
 
     // 画像のメタデータを取得
-    const islandImageCollection = new IslandImageCollection(uid);
+    const islandImageCollection = new IslandImageCollection(islandId);
     const imageList = await islandImageCollection.getThumbnails();
 
     // 質問を取得
-    const islandQuestionCollection = new IslandQuestionCollection(uid);
+    const islandQuestionCollection = new IslandQuestionCollection(islandId);
     const questionList =
       await islandQuestionCollection.getQuestionsWithAnswers();
 
     return {
       result: true,
-      islandInfo: docSnap.data(),
+      islandInfo: islandData,
       imageList: imageList,
       questionList: await Promise.all(questionList),
     };
