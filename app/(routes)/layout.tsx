9999c11f6938010e6605c2db9571b2ce71516375
kamonohashiK <kamonohashiy@gmail.com>
2023-10-15 06:02:19 +0900

    メタタグの設定

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,23 +1,25 @@
 "use client";
-import '../_styles/globals.css'
-import { Inter } from 'next/font/google'
+import "../_styles/globals.css";
+import { Inter } from "next/font/google";
 import Header from "../_components/header/header";
 import { store } from "../_store/store";
 import { Provider } from "react-redux";
-import CommonDialog from '../_components/util/common_dialog';
-
-const inter = Inter({ subsets: ['latin'] })
-
-// Reduxのstoreを使うためmetadataを一旦削除
+import CommonDialog from "../_components/util/common_dialog";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="ja">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="description" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>title</title>
+      </head>
+      <body>
         <Provider store={store}>
           <Header />
           <CommonDialog />
