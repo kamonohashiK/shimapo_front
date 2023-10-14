2728936d228e64e7a7c684922b23a33585d3c3de
kamonohashiK <kamonohashiy@gmail.com>
2023-10-14 14:40:01 +0900

    画像一覧ページのイメージ作成

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,6 +7,7 @@ import firebase_app from "@/firebase/config";
 import { getAuth } from "@firebase/auth";
 import { useDispatch } from "react-redux";
 import { setLoginInfo, unmountLoginInfo } from "../_store/userSlice";
+import CustomImageList from "../_components/image_list";
 
 export default function Home() {
   const googleMapApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY;
@@ -35,13 +36,15 @@ export default function Home() {
     return () => unsubscribe();
   }, []);
 
+  const isMap = true;
+
   return (
     <Grid container direction="row" spacing={2}>
       <Grid item xs={4}>
         <Sidebar />
       </Grid>
       <Grid item xs={8}>
-        <IslandsMap apiKey={googleMapApiKey} />
+        {isMap ? <IslandsMap apiKey={googleMapApiKey} /> : <CustomImageList />}
       </Grid>
     </Grid>
   );
