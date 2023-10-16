096f6a810d672bf7406cab66242ded19f90154f5
kamonohashiK <kamonohashiy@gmail.com>
2023-10-16 09:33:54 +0900

    #78 ページアクセス時にログイン状態をチェックする処理を追加

--- app/(routes)/mypage/page.tsx
+++ app/(routes)/mypage/page.tsx
@@ -5,9 +5,38 @@ import Sidebar from "../../_components/sidebar/_";
 import { UserProfile } from "@/app/_components/mypage/user_profile";
 import { UserStats } from "@/app/_components/mypage/user_stats";
 import { NotificationTab } from "@/app/_components/mypage/notification_tab";
+import { setLoginInfo, unmountLoginInfo } from "@/app/_store/slices/userSlice";
+import firebase_app from "@/firebase/config";
+import { getAuth } from "@firebase/auth";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/navigation";
 
 export default function MyPage() {
-  //TODO: マイページアクセス時に左サイドバーの状態を切り替えるようにする
+  const { push } = useRouter();
+  const auth = getAuth(firebase_app);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    // ログイン状態を検知する TODO: 共通化する
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(
+          setLoginInfo({
+            displayName: user.displayName!,
+            userId: user.uid,
+            photoUrl: user.photoURL!,
+            loggedIn: true,
+          })
+        );
+      } else {
+        dispatch(unmountLoginInfo());
+        push("/");
+      }
+    });
+
+    // コンポーネントがアンマウントされたときにunsubscribeする
+    return () => unsubscribe();
+  }, []);
 
   return (
     <Container sx={{ maxHeight: "100vh", overflowY: "hidden" }}>
