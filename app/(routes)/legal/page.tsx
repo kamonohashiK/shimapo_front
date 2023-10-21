c511ff0dc93f2cf0d14d529f13f862f2790e3399
kamonohashiK <kamonohashiy@gmail.com>
2023-10-22 07:51:58 +0900

    #111 リーガルページのレイアウトをPCとモバイルとで切り分け

--- app/(routes)/legal/page.tsx
+++ app/(routes)/legal/page.tsx
@@ -1,24 +1,7 @@
-import { PrivacyPolicy } from "@/app/_components/legal/privacy_policy";
-import { TermsOfUse } from "@/app/_components/legal/terms_of_use";
-import Sidebar from "@/app/_components/sidebar/_";
-import { Container, Divider, Grid, Typography } from "@mui/material";
+import { LegalPageMobile } from "@/app/_components/page/mobile/legal";
+import { LegalPagePC } from "@/app/_components/page/pc/legal";
 
 export default function LegalPage() {
-  return (
-    <Container sx={{ maxHeight: "100vh" }}>
-      <Grid container direction="row" spacing={2}>
-        <Grid item xs={4}>
-          <Sidebar />
-        </Grid>
-        <Grid item xs={8} id="content">
-          <Container fixed>
-            <TermsOfUse />
-            <Divider sx={{ margin: 2 }} />
-            <PrivacyPolicy />
-            <Typography paragraph>©️ 2023 Kouki Kadoya</Typography>
-          </Container>
-        </Grid>
-      </Grid>
-    </Container>
-  );
+  const isMobile = true;
+  return <>{isMobile ? <LegalPageMobile /> : <LegalPagePC />}</>;
 }
